5b18153e42acf5aca60fbc9600e1af2d4e6606ee
Dylan Fairchild <dylan.b.fairchild@gmail.com>
2022-02-01 20:53:20 -0800

    added implementation for trip LocationPage so we get HTML for that generate a multi-level folder structure when building the site

--- src/trip/cambodia/mod.rs
+++ src/trip/cambodia/mod.rs
@@ -1,7 +1,85 @@
 use std::error::Error;
 
+use crate::trip::{ftr, hdr, lcnt, lftr, lhdr, sec, LocationPage, TripDayPage};
 use crate::{Htmlize, Page};
-use crate::trip::{hdr, ftr, sec, TripDayPage};
+
+pub fn cambodia() -> Result<(Page, Vec<Box<dyn Htmlize>>), Box<dyn Error>> {
+    Ok((
+        Page::Location(LocationPage::new("cambodia.html", "atw/cambodia")),
+        vec![
+            lhdr("Cambodia"),
+            lcnt(vec![
+                (
+                    "siem_reap_arrival.html",
+                    "Siem Reap Arrival",
+                    "siem_reap_arrival/night_lotus.jpg",
+                    "After a few days in Ho Chi Minh City we were ready to visit some
+                    of the more rural areas of South East Asia. Up first on our list,
+                    the Angkor temple complex in Siem Reap. On our travel day we did
+                    not visit the temple complex, but we did explore the local area
+                    with a guided bicycle tour from our hostel through the lotus
+                    fields and to nearby Buddhist temple.",
+                ),
+                (
+                    "siem_reap_day_2.html",
+                    "Siem Reap Day 2",
+                    "siem_reap_day_2/sunrise_ish.jpg",
+                    "Our first full day in Siem Reap we did the morning Ankor tour
+                    through our hostel. This hit the main attractions like Angkor Wat,
+                    Bayon, Ta Prohm, and Banteay Kdei. It really feels like you are
+                    Indiana Jones exploring these temples, absolutely stunning. We
+                    finished off our day with a double-dinner of wood-fired fresh
+                    pineapple pizza and a Khmer curry on Pub Street.",
+                ),
+                (
+                    "siem_reap_day_3.html",
+                    "Siem Reap Day 3",
+                    "siem_reap_day_3/tree_preah.jpg",
+                    "After getting a taste of the adventure with the morning tour, we
+                    decided to book the afternoon tour and hit most of the rest of the
+                    temples. We also had a nice night on pub street with the $0.50
+                    drafts and live music!",
+                ),
+                (
+                    "siem_reap_day_4.html",
+                    "Siem Reap Day 4",
+                    "siem_reap_day_4/restaurant_steph_drinks.jpg",
+                    "After a few exhausting days exploring the temple complex we
+                    decided to take a day off. Today we got the scattered downpours
+                    typical of the region, so we explored around town for some food
+                    and otherwise took the opportunity to work on our posts and books."
+                ),
+                (
+                    "phnom_penh_arrival.html",
+                    "Phnom Penh Arrival",
+                    "phnom_penh_arrival/noodle_making.jpg",
+                    "Today was our travel day to Phnom Penh, and our first non-plane
+                    travel day of the trip. We took a bus cross-country from Siem Reap
+                    to Phnom Penh and got to soak in the sights of the Cambodian
+                    countryside. Altogether it was a really nice bus trip, with a
+                    couple stops but nothing too egregious and tourist trappy. When we
+                    arrived in Phnom Penh we checked out the rooftop pool on our hotel
+                    (super cool!) and had one of the best meals of the trip, David's
+                    Homemade Noodles."
+                ),
+                (
+                    "phnom_penh_day_2.html",
+                    "Phnom Penh Day 2",
+                    "phnom_penh_day_2/dylan_pool.jpg",
+                    "Today we decided to learn about Khmer Rouge, an important period
+                    in Cambodian history. Unfortunately, also an extremely dark period
+                    of genocide. After visiting the Tuol Sleng Genocide Museum we hit
+                    some of the other major attractions in town a long the riverside
+                    and made our way to the famous Friends Restauraunt, where Steph
+                    ATE BUGS! We spent the afternoon fully enjoying the rooftop pool
+                    at the hotel, from sunbathing to the light show on the city
+                    skyline at night."
+                )
+            ]),
+            lftr(),
+        ],
+    ))
+}
 
 pub fn siem_reap_arrival() -> Result<(Page, Vec<Box<dyn Htmlize>>), Box<dyn Error>> {
     Ok((
--- src/trip/mod.rs
+++ src/trip/mod.rs
@@ -4,17 +4,298 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
-use crate::{Htmlize, Page};
+use crate::{root, Htmlize, Page};
+
+fn path_str_to_components(path: &str) -> Vec<String> {
+    let mut components = Vec::new();
+
+    // Paths that start with a "/" character will get broken up such
+    // that the first entry in the return value is an empty string.
+    let mut component = "".to_string();
+
+    for c in path.chars() {
+        if c == '/' {
+            components.push(component.clone());
+            component.clear();
+        } else {
+            component.push(c);
+        }
+    }
+
+    if component.len() > 0 {
+        components.push(component);
+    }
+
+    components
+}
+
+fn slice_before_match_from_end_ci<'a>(s: &'a str, m: &str) -> &'a str {
+    let i = s.len() - m.len();
+
+    //TODO: Can lowercasing a string change the length of it?
+    //      Is there a better way of doing string operations like this in Rust?
+    // Should not affect this project, but good things to research.
+    let l = s.to_lowercase();
+    let lm = m.to_lowercase();
+    if l.len() > i && &l[i..] == lm {
+        return &s[..i];
+    }
+
+    s
+}
+
+fn htmlize_boilerplate_header_and_navigation(
+    page: &Page,
+    output: &mut BufWriter<File>,
+) -> Result<(), Box<dyn Error>> {
+    // The following code outputs the navigation tree at the top of the page.
+    // This is a bit tricky, so here is what it is intended to do:
+    // Page given is for siem_reap_arrival.html
+    // Path given on the page is... atw/cambodia
+    // Navigation should look like... /home/atw/cambodia/siem_reap_arrival, all links except the last
+    // Name from page is... siem_reap_arrival.html
+    // Location on the server is... /atw/cambodia/siem_reap_arrival.html
+    //
+    // Components array from path_str_to_components will look like:
+    // 0 - atw
+    // 1 - cambodia
+    // We want to output... /home/atw/cambodia/siem_reap_arrival
+    // So, the first block takes care of /home, which is always a static link for any given TripDayHeader.
+    // The middle block takes care of /atw/cambodia, which we get from our TripDayPage path.
+    // The last block takes care of /siem_reap_arrival, which we get from our TripDayPage name.
+
+    // This beginning is just the simple boilerplate to set up the page and the navigation.
+    output.write_all(
+        &format!(
+            r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>fairchild.tech</title>
+        <link rel="stylesheet" type="text/css" href="{}css/simple.css" />
+    </head>
+    <body>
+        <div class="navbar-container">
+            <div class="navbar">fairchild.tech</div>
+            <div class="navbar-path">"#,
+            root()
+        )
+        .into_bytes(),
+    )?;
+
+    // "First block" - takes care of /home.
+    output.write_all(
+        &format!(
+            r#"
+                /<a href="{}index.html">home</a>"#,
+            root()
+        )
+        .into_bytes(),
+    )?;
+    // "Middle block" - takes care of tricky variable navigation.
+    let components = path_str_to_components(page.path());
+    let mut path_to_component = String::new();
+    for component in components.iter() {
+        path_to_component = path_to_component + component + "/";
+        // With the exception of index.html at home, there is always a file named after
+        // the folder that it is in which acts as the landing page for that folder.
+        let path_to_html = root() + &path_to_component + component + ".html";
+        output.write_all(
+            &format!(
+                r#"
+                /<a href="{}">{}</a>"#,
+                path_to_html, component
+            )
+            .into_bytes(),
+        )?;
+    }
+    // "Last block" - takes care of the page we are currently on.
+    // This needs to have the ".html" removed from the name since we specify
+    // page names with the extension on creation.
+    let name = slice_before_match_from_end_ci(page.name(), ".html");
+    output.write_all(
+        &format!(
+            r#"
+                /{}"#,
+            name
+        )
+        .into_bytes(),
+    )?;
+
+    // Close off the common navigation bar / header HTML.
+    output.write_all(
+        br#"
+            </div>
+            <div style="clear: both;"></div>
+        </div>"#,
+    )?;
+
+    Ok(())
+}
 
 pub struct LocationPage {
     pub name: String,
+    pub path: String,
 }
 
 impl LocationPage {
-    pub fn new(name: &str) -> LocationPage {
+    pub fn new(name: &str, path: &str) -> LocationPage {
         LocationPage {
             name: name.to_string(),
+            path: path.to_string(),
         }
+    }
+}
+
+pub struct LocationHeader {
+    title: String,
+}
+
+impl LocationHeader {
+    pub fn new(title: &str) -> LocationHeader {
+        LocationHeader {
+            title: title.to_string(),
+        }
+    }
+}
+
+impl Htmlize for LocationHeader {
+    fn htmlize(&self, page: &Page, output: &mut BufWriter<File>) -> Result<(), Box<dyn Error>> {
+        htmlize_boilerplate_header_and_navigation(page, output)?;
+
+        output.write_all(
+            &format!(
+                r#"
+        <div class="region-menu">
+            <div class="page-title">
+                <div class="page-title-text-container">
+                    <div class="page-title-text">{}</div>
+                </div>
+            </div>
+            <div class="spacer"></div>
+        "#,
+                self.title
+            )
+            .into_bytes(),
+        )?;
+
+        Ok(())
+    }
+}
+
+pub struct LocationFooter {}
+
+impl Htmlize for LocationFooter {
+    fn htmlize(&self, _: &Page, output: &mut BufWriter<File>) -> Result<(), Box<dyn Error>> {
+        output.write_all(
+            br#"
+        </div>
+    </body>
+</html>"#,
+        )?;
+
+        Ok(())
+    }
+}
+
+struct LocationContentSection {
+    link: String,
+    title: String,
+    image: String,
+    text: String,
+}
+
+pub struct LocationContent {
+    content: Vec<LocationContentSection>,
+}
+
+impl LocationContent {
+    pub fn new(content: Vec<(&str, &str, &str, &str)>) -> LocationContent {
+        LocationContent {
+            content: content
+                .iter()
+                .map(|s| LocationContentSection {
+                    link: s.0.to_string(),
+                    title: s.1.to_string(),
+                    image: s.2.to_string(),
+                    text: s.3.to_string(),
+                })
+                .collect(),
+        }
+    }
+
+    fn htmlize_image(
+        &self,
+        _: &Page,
+        output: &mut BufWriter<File>,
+        c: &LocationContentSection,
+    ) -> Result<(), Box<dyn Error>> {
+        let image_path = root() + "from-bucket/" + &c.image;
+        output.write_all(
+            &format!(
+                r#"
+                <img class="region-menu-card-image" src="{}" />"#,
+                image_path
+            )
+            .into_bytes(),
+        )?;
+        Ok(())
+    }
+    fn htmlize_text(
+        &self,
+        _: &Page,
+        output: &mut BufWriter<File>,
+        c: &LocationContentSection,
+    ) -> Result<(), Box<dyn Error>> {
+        output.write_all(
+            &format!(
+                r#"
+                <div class="region-menu-text-box">
+                    <div class="region-menu-title">{}</div>
+                    <div class="region-menu-text">{}</div>
+                </div>"#,
+                c.title, c.text
+            )
+            .into_bytes(),
+        )?;
+        Ok(())
+    }
+}
+
+impl Htmlize for LocationContent {
+    fn htmlize(&self, page: &Page, output: &mut BufWriter<File>) -> Result<(), Box<dyn Error>> {
+        let mut left = true;
+        for c in self.content.iter() {
+            let link = root() + page.path() + "/" + &c.link;
+
+            output.write_all(
+                &format!(
+                    r#"
+        <a class="card-link" href="{}">
+            <div class="region-menu-card">"#,
+                    link
+                )
+                .into_bytes(),
+            )?;
+
+            if left {
+                self.htmlize_image(page, output, c)?;
+                self.htmlize_text(page, output, c)?;
+            } else {
+                self.htmlize_text(page, output, c)?;
+                self.htmlize_image(page, output, c)?;
+            }
+            left = !left;
+
+            output.write_all(
+                br#"
+            </div>
+        </a>
+        <div class="spacer"></div>"#,
+            )?;
+        }
+
+        Ok(())
     }
 }
 
@@ -46,114 +327,14 @@ impl TripDayHeader {
     }
 }
 
-fn path_str_to_components(path: &str) -> Vec<String> {
-    let mut components = Vec::new();
-
-    // Paths that start with a "/" character will get broken up such
-    // that the first entry in the return value is an empty string.
-    let mut component = "".to_string();
-
-    for c in path.chars() {
-        if c == '/' {
-            components.push(component.clone());
-            component.clear();
-        }
-        else {
-            component.push(c);
-        }
-    }
-
-    if component.len() > 0 {
-        components.push(component);
-    }
-
-    components
-}
-
-fn path_to_component_html(components: &Vec<String>, index: usize) -> String {
-    for i in 0..index {
-
-    }
-
-    "".to_string()
-}
-
-fn slice_before_match_from_end_ci<'a>(s:  &'a str, m: &str) -> &'a str {
-    let mut i = s.len() - m.len();
-
-    //TODO: Can lowercasing a string change the length of it?
-    //      Is there a better way of doing string operations like this in Rust?
-    // Should not affect this project, but good things to research.
-    let l = s.to_lowercase();
-    let lm = m.to_lowercase();
-    if l.len() > i && &l[..l.len()-i] == lm{
-        return &s[s.len()-i..];
-    }
-
-    s
-}
-
 impl Htmlize for TripDayHeader {
     fn htmlize(&self, page: &Page, output: &mut BufWriter<File>) -> Result<(), Box<dyn Error>> {
-        // The following code outputs the navigation tree at the top of the page.
-        // This is a bit tricky, so here is what it is intended to do:
-        // Page given is for siem_reap_arrival.html
-        // Path given on the page is... atw/cambodia
-        // Navigation should look like... /home/atw/cambodia/siem_reap_arrival, all links except the last
-        // Name from page is... siem_reap_arrival.html
-        // Location on the server is... /atw/cambodia/siem_reap_arrival.html
-        //
-        // Components array from path_str_to_components will look like:
-        // 0 - atw
-        // 1 - cambodia
-        // We want to output... /home/atw/cambodia/siem_reap_arrival
-        // So, the first block takes care of /home, which is always a static link for any given TripDayHeader.
-        // The middle block takes care of /atw/cambodia, which we get from our TripDayPage path.
-        // The last block takes care of /siem_reap_arrival, which we get from our TripDayPage name.
-
-        // This beginning is just the simple boilerplate to set up the page and the navigation.
-        output.write_all(
-                br#"
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>fairchild.tech</title>
-        <link rel="stylesheet" type="text/css" href="css/simple.css" />
-    </head>
-    <body>
-        <div class="navbar-container">
-            <div class="navbar">fairchild.tech</div>
-            <div class="navbar-path">"#)?;
-
-
-        // "First block" - takes care of /home.
-        output.write_all(br#"
-                /<a href="index.html">home</a>"#)?;
-        // "Middle block" - takes care of tricky variable navigation.
-        let components = path_str_to_components(page.path());
-        let mut path_to_component = String::new();
-        for component in components.iter() {
-            path_to_component = path_to_component + component + "/";
-            // With the exception of index.html at home, there is always a file named after
-            // the folder that it is in which acts as the landing page for that folder.
-            let path_to_html = String::new() + &path_to_component + component + ".html";
-            output.write_all(&format!(r#"
-                /<a href="{}">{}</a>"#, path_to_html, component).into_bytes())?;
-        }
-        // "Last block" - takes care of the page we are currently on.
-        // This needs to have the ".html" removed from the name since we specify
-        // page names with the extension on creation.
-        let name = slice_before_match_from_end_ci(page.name(), ".html");
-        output.write_all(&format!(r#"
-                /{}"#, name).into_bytes())?;
+        htmlize_boilerplate_header_and_navigation(page, output)?;
 
         // OK, that was it for the navigation, now we can just close out the simple pieces.
         output.write_all(
             &format!(
                 r#"
-            </div>
-            <div style="clear: both;"></div>
-        </div>
         <div class="page-container">
             <div class="page-title">
                 <div class="page-title-text-container">
@@ -204,7 +385,7 @@ impl TripDayContentSection {
         index: usize,
     ) -> Result<(), Box<dyn Error>> {
         if index >= self.content.len() {
-            return Ok(())
+            return Ok(());
         }
 
         if self.is_image(index) {
@@ -216,7 +397,7 @@ impl TripDayContentSection {
                 )?;
             }
 
-            let path = String::new() + "from-bucket/" + page.resource_path() + "/";
+            let path = root() + "from-bucket/" + page.resource_path() + "/";
 
             // Output the image content.
             output.write_all(
@@ -238,7 +419,7 @@ impl TripDayContentSection {
         } else if self.is_video(index) {
             // Output video content.
 
-            let path = String::new() + "from-bucket/" + page.resource_path() + "/";
+            let path = root() + "from-bucket/" + page.resource_path() + "/";
 
             output.write_all(
                 &format!(
@@ -340,4 +521,13 @@ pub fn ftr() -> Box<TripDayFooter> {
 }
 pub fn sec(title: &str, content: Vec<&str>) -> Box<TripDayContentSection> {
     Box::new(TripDayContentSection::new(title, content))
+}
+pub fn lhdr(title: &str) -> Box<LocationHeader> {
+    Box::new(LocationHeader::new(title))
+}
+pub fn lftr() -> Box<LocationFooter> {
+    Box::new(LocationFooter {})
+}
+pub fn lcnt(content: Vec<(&str, &str, &str, &str)>) -> Box<LocationContent> {
+    Box::new(LocationContent::new(content))
 }
